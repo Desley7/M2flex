6e2fb18f2887e45a1c4206103bfe4606d6f0f8d2
Desley Puttileihalat <d.puttileihalat@icloud.com>
2024-01-19 12:16:05 +0100

    pionen

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 let uiWindow = createRect(600,200,300,300);
+let images ={};
+ 
 
 const gamestate_start=0;
 const gamestate_ingame=1;
 const gamestate_gameover=2;
-
+ 
 const ingamestate_start=0;
 const ingamestate_roll=1
 const ingamestate_end=0;
+
+let gameState = gamestate_start;
+let ingameState = ingamestate_start
 
 let boardPositionSize= 50;
 let pawnPosition= [];
 let boardPositions=[];
 let playerAmountButtons = [];
-
+ 
 let canvas = document.getElementById("canvas");
 let g = canvas.getContext("2d");
 
@@ -32,105 +37,118 @@ function createRect(x,y,w,h)
  {
     g.fillStyle = "lightgray";
     g.fillRect(0,0, canvas.width, canvas.height);
-
+ 
  }
-
+ 
  function draw()
 {
     clearCanvas();
-    for(let i =0 ; i<boardPositions.length;i++)
-    {
-        let pos = boardPositions[i];
-
-        g.fillStyle  = "#004400";
-        g.fillRect(pos.x,pos.y,pos.w,pos.h);
-        g.fillStyle  = "#FFFFFF";
-        g.fillText((i+1)+"",pos.x,pos.y+20);
-    }
+    drawGameStart();
+    drawIngame();
 }
 function createBoardPositions() {
     let x = 0;
     let y = canvas.height -boardPositionSize;
     let path = [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1];
-
+ 
     for (let i = 0; i < path.length; i++) {
         if (path[i] == 1) {
            
-            x += boardPositionSize; 
+            x += boardPositionSize;
         } else if (path[i] == 3) {
-          
-            x -= boardPositionSize; 
+         
+            x -= boardPositionSize;
         } else if (path[i] == 0) {
-            
-            y -= boardPositionSize; 
+           
+            y -= boardPositionSize;
         }
         boardPositions.push(createRect(x, y, boardPositionSize, boardPositionSize));
     }
 }
- function initGame()
- {
-    createBoardPositions
- }
- let button = createRect(uiWindow.x + 5 + (i * (50 + 10)), uiWindow.y + 50, 50, 50);
+function initGame(){
+  createBoardPositions();
+  for (let index = 0; index < 4; index++) {
+    let button = 
+    createRect(uiWindow.x+5 + (index*50),uiWindow.y+50,50,50)
+button.playerAmount=index+1;
+playerAmountButtons.push(button);    
+  }
+}
+function drawIngame(){
+  for(let i =0 ; i<boardPositionSize.length;i++)
+  {
+    let pos = boardPositions[i];
 
- // Voeg bovenaan in je bestand de volgende variabelen toe:
- {let gamestate_start = 0;
- let gamestate_ingame = 1;
- let gamestate_gameover = 2;
- let gameState = gamestate_start;}
-
-// Voeg de drawGameStart, drawIngame en drawGameOver functies toe:
-
+    g.fillStyle = "#004400";
+    g.fillRect(pos.x,pos.y,pos.w,pos.h);
+    g.fillStyle = "#FFFFFF"
+    g.fillText((i+1)+"",pos.x,pos.y+20);
+  }
+}
 function drawGameStart() {
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  g.fillStyle = "#000";
+  g.font = "20px Arial";
+  g.fillText("Click the amount of players to start", canvas.width / 2 - 180, 50);
 
-    ctx.fillStyle = "white";
-    ctx.font = "30px Arial";
-    ctx.fillText("Click the amount of players to start", 50, 50);
 
-    // Teken de buttons
-    for (let i = 0; i < playerAmountButtons.length; i++) {
-        ctx.fillStyle = "blue"; // Pas de kleur aan indien nodig
-        ctx.fillRect(playerAmountButtons[i].x, playerAmountButtons[i].y, playerAmountButtons[i].width, playerAmountButtons[i].height);
+  for (let i = 0; i < playerAmountButtons.length; i++) {
+      if (i % 2 === 0) {
+          g.fillStyle = "#3498db";
+      } else {
+          g.fillStyle = "#e74c3c"; 
+      }
+      
+      g.fillRect(playerAmountButtons[i].x, playerAmountButtons[i].y, playerAmountButtons[i].w, playerAmountButtons[i].h);
+      g.fillStyle = "#fff";
+      g.font = "16px Arial";
+      const buttonText = playerAmountButtons[i].playerAmount.toString();
+      const textX = playerAmountButtons[i].x + (playerAmountButtons[i].w / 2) - g.measureText(buttonText).width / 2;
+      const textY = playerAmountButtons[i].y + (playerAmountButtons[i].h / 2) + 6;
+      g.fillText(buttonText, textX, textY);
+        g.drawImage(images["pawn"+i+".png"], playerAmountButtons[i].x, playerAmountButtons[i].y); 
+  }
+}
 
-        ctx.fillStyle = "white";
-        ctx.font = "20px Arial";
-        ctx.fillText(playerAmountButtons[i].playerAmount, playerAmountButtons[i].x + 20, playerAmountButtons[i].y + 30);
+  function loadImages()
+{
+    let sources = [
+        "img/dice1.png", "img/dice2.png", "img/dice3.png", "img/dice4.png", "img/dice5.png", "img/dice6.png",
+        "img/pawn0.png", "img/pawn1.png", "img/pawn2.png", "img/pawn3.png", 
+        "img/snakes.png", 
+        "img/trophy.png", 
+        "img/window.png", 
+    ];
+    
+    let scope = this;
+
+    let loaded = 0;
+    for (let i = 0; i < sources.length; i++)
+    {
+        let img = new Image();
+
+
+        img.onload = function ()
+        {
+            loaded++;
+            if (loaded == sources.length)
+            {
+                imagesLoaded();
+            }
+        };
+        img.src = sources[i];
+
+        images[ sources[i].replace("img/","")] = img;
+
     }
 }
 
-function drawIngame() {
-    // Teken het bord of andere elementen voor de 'ingame' state
-    // Deze code zal hier worden geplaatst wanneer je het implementeert
-}
+function imagesLoaded() {
+    initGame();
+    draw(); 
+  }
 
-function drawGameOver() {
-    // Teken het game-over scherm of andere elementen voor de 'gameover' state
-    // Deze code zal hier worden geplaatst wanneer je het implementeert
-}
 
-// Pas de draw functie aan:
+loadImages();
+draw();
 
-function draw() {
-    if (gameState === gamestate_start) {
-        drawGameStart();
-    } else if (gameState === gamestate_ingame) {
-        drawIngame();
-    } else if (gameState === gamestate_gameover) {
-        drawGameOver();
-    }
-}
 
-// Voeg de volgende regels toe onder de initGame-functie:
-
-// Stel de playerAmount in voor elke button
-for (let i = 0; i < playerAmountButtons.length; i++) {
-    playerAmountButtons[i].playerAmount = i + 1;
-}
-
-// Voeg de volgende regels toe om de draw-functie aan te roepen:
-initGame(); // Roep initGame aan om de buttons en andere initialisaties uit te voeren
-draw(); // Roep draw aan om het juiste scherm te tekenen
-
- 
